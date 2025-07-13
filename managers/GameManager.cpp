7d2f857c1e913f0dcb508a0eef55dff8bef3ef4e
lzh1B <2742530861@qq.com>
2025-07-14 07:26:23 +0800

    改变了目录结构 创建Classes目录存放原有文件 创建Resources目录存放原有资源 将Ap'p'Delegate类和HelloWorldScene类作为启动项存放到Classes/configs目录下 resources改为Resources

--- managers/GameManager.cpp
+++
@@ -1,8 +0,0 @@
-#include "GameManager.h"
-
-std::vector<CardData>* GameManager::getPlayfieldCards() {
-	return _playfieldCards;
-}
-std::vector<CardData>* GameManager::getStackCards() {
-	return _stackCards;
-}
--- models/CardData.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-//从配置文件中加载到的卡牌数据
-struct CardData {
-    int cardFace;
-    int cardSuit;
-    int posX;
-    int posY;
-};
